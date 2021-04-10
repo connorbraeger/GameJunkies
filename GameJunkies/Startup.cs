b5fc0ccbc262c3c9a01be59e8452a9cd07015cc0
Connor Braeger <cjbraeger@gmail.com>
2021-04-10 18:20:48 -0400

    Add project files.

---
+++ GameJunkies/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GameJunkies.Startup))]
+namespace GameJunkies
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

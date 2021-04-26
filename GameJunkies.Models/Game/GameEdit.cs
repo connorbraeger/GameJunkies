e207666d13b2ed7e4419e0d2517e4c8ae87125ff
Connor Braeger <cjbraeger@gmail.com>
2021-04-25 20:38:07 -0400

    Game Models finished

---
+++ GameJunkies.Models/Game/GameEdit.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameJunkies.Models.Game
+{
+    public class GameEdit
+    {
+        public int? GameId { get; set; }
+     
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public string ParentalRating { get; set; }
+        [Required]
+        public int? GenreId { get; set; }
+        [Required]
+        public int? DeveloperId { get; set; }
+        [Required]
+        public int? PublisherId { get; set; }
+        [Required, DisplayName("Release Date")]
+        public DateTimeOffset ReleaseDate { get; set; }
+    }
+}

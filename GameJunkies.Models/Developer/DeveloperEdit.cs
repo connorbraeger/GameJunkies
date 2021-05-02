53e218995fa996ca1398012f483bdfbe7b8e409f
Connor Braeger <cjbraeger@gmail.com>
2021-05-02 18:48:04 -0400

    Added developer controller

--- GameJunkies.Models/Developer/DeveloperEdit.cs
+++ GameJunkies.Models/Developer/DeveloperEdit.cs
@@ -18,6 +18,6 @@ namespace GameJunkies.Models.Developer
         [Required]
         public string Country { get; set; }
         [Required]
-        public float Rating { get; set; }
+        public float? Rating { get; set; }
     }
 }
---
+++ GameJunkies.WebMVC/Controllers/DeveloperController.cs
@@ -0,0 +1,98 @@
+﻿using GameJunkies.Models.Developer;
+using GameJunkies.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GameJunkies.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class DeveloperController : Controller
+    {
+        // GET: Developer
+        public ActionResult Index()
+        {
+            var service = new DeveloperService();
+            var model = service.GetDevelopers();
+            return View(model);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(DeveloperCreate model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var service = new DeveloperService();
+
+            if (service.CreateDeveloper(model))
+            {
+                TempData["SaveResult"] = "Your developer was added.";
+                return RedirectToAction("Index");
+            }
+            ModelState.AddModelError("", "Developer could not be added.");
+            return View(model);
+
+        }
+        public ActionResult Details(int id)
+        {
+            var service = new DeveloperService();
+            var model = service.GetDeveloperById(id);
+            if (model == null)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+        public ActionResult Edit(int id)
+        {
+            var service = new DeveloperService();
+            var detail = service.GetDeveloperById(id);
+            var model = new DeveloperEdit()
+            {
+                DeveloperId = detail.DeveloperId,
+                Name = detail.Name,
+                CompanySize = detail.CompanySize,
+                Country = detail.Country,
+                Rating = detail.Rating
+            };
+            return View(model);
+
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, DeveloperEdit model)
+        {
+            if (!ModelState.IsValid) return View(model);
+            if (model.DeveloperId != id)
+            {
+                ModelState.AddModelError("", "Id Mismatch");
+                return View(model);
+            }
+            var service = new DeveloperService();
+            if (service.UpdateDeveloper(model))
+            {
+                TempData["SaveResult"] = "The developer was updated.";
+                return RedirectToAction("Index");
+            }
+            ModelState.AddModelError("", "Developer could not be updated.");
+            return View(model);
+        }
+        [ActionName("Delete")]
+        public ActionResult Delete(int id)
+        {
+            var service = new DeveloperService();
+            var model = service.GetDeveloperById(id);
+
+            return View(model);
+        }
+    }
+}
